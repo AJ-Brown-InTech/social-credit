fe9f6aef3fd75eb50eba2fc2e6c0de5791480cb7
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-11-18 23:14:54 -0600

    navigation container

---
+++ Pages/Home.js
@@ -0,0 +1,18 @@
+import {Component} from 'react'
+import {Text, View} from 'react-native'
+
+class Home extends Component {
+render(){
+    return(
+        <Text>HIjnjnjnjnjnjnjj{console.log(this.props)}</Text>
+    )
+}
+
+}
+// const Home = ({ navigation }) => {
+//     return (
+//      <Text>HIjnjnjnjnjnjnjj</Text>
+//     );
+//   };
+
+  export default Home
