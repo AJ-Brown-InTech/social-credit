e4ff119414b1216300dd8a47caeb725cc02ff1ce
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-11-27 13:15:30 -0600

    backg image

--- styles/styles.js
+++ styles/styles.js
@@ -33,6 +33,7 @@ const styles =  StyleSheet.create({
             width: deviceWidth,
             height: deviceHeight,
             justifyContent: 'space-between'
+            
           }
 })      
 
