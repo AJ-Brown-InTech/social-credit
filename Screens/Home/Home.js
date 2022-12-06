d106e71aba4347f0fed1bc1f9322d480c95189a0
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-12-06 14:35:19 -0600

    icon added for pages need a custom header

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
 import {Text, View} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from '../../styles/styles'
 //Main User screens
 import Activity from './Activity'
@@ -10,34 +11,50 @@ import Profile from './Profile'
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Activity" component={Activity} />
-      <Tab.Screen name="Discover" component={Discover} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
-}
-
-
-
 class Home extends Component {
 render(){
     return(
-        // <View style={{
-        //     flex: 1, 
-        //     alignItems: 'center',
-        //     justifyContent: 'center', 
-        // }}>
-        //     <Text >Home Page</Text>
-        //    </View>
-    <Tab.Navigator initialRouteName="Feed">
-      <Tab.Screen name="Activity" component={Activity}  />
-      <Tab.Screen name="Discover" component={Discover} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Profile" component={Profile} />
+   
+    <Tab.Navigator 
+    initialRouteName="Feed"
+    tabBarOptions={{
+      activeTintColor: '#99b9ad',
+      tabBarPosition: 'bottom',
+      swipeEnabled: true,
+      lazy: false,
+      removeClippedSubviews: false,
+      showLabel: false,
+    }}>
+      <Tab.Screen name="Activity" component={Activity}  
+      options={{
+        tabBarLabel: 'Activity',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="bell" color={color} size={size} />
+        ),
+        tabBarBadge: 3,
+      }}/>
+      <Tab.Screen name="Discover" component={Discover} 
+      options={{
+        tabBarLabel: 'Discover',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="magnify" color={color} size={size} />
+        ),
+        headerShown: false
+      }}/>
+      <Tab.Screen name="Feed" component={Feed} 
+      options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }} />
+      <Tab.Screen name="Profile" component={Profile} 
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}/>
     </Tab.Navigator>
             )
     }
